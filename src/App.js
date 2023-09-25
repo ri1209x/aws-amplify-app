1bc747d27a5fb42f2bd2dbe1894bca107ab40db5
Ryuga <i.ryuga917@gmail.com>
2023-09-25 15:59:41 +0900

    deploy

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { Amplify }  from "aws-amplify";
+import {withAuthenticator} from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+
+import awsExports from "./aws-exports";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>Hello React with AWS</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
